fce48c01ea51b39c5ece20d7e86949850d6e84c1
Lihao Lu <lihaolu@amazon.com>
2019-06-26 15:31:52 -0700

    one hour break reminder

---
+++ one_hour_break.sh
@@ -0,0 +1,9 @@
+sleep 90
+say "You should move a bit"
+sleep 90
+say "You should move a bit"
+sleep 90
+say "You should move a bit"
+sleep 90
+echo "It's time for a break" | terminal-notifier -sound default
+say "It's time for a break"
