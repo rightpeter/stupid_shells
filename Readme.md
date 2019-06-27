2e2df379071c94f6957f7790e5834e6cebdfea7b
Lihao Lu <lihaolu@amazon.com>
2019-06-27 11:46:31 -0700

    Refactor

--- Readme.md
+++ Readme.md
@@ -3,3 +3,7 @@
 ## One Hour Break Reminder
 
 	brew install terminal-notifier
+	# Add this script to /usr/local/bin
+	ln -s $(pwd)/one_hour_break.sh /usr/local/bin/one_hour_break
+
+Then, you can call `one_hour_break &` at the begining of your one hour session.
--- one_hour_break.sh
+++ one_hour_break.sh
@@ -1,9 +1,14 @@
-sleep 90
-say "You should move a bit"
-sleep 90
-say "You should move a bit"
-sleep 90
-say "You should move a bit"
-sleep 90
-echo "It's time for a break" | terminal-notifier -sound default
+#!/usr/bin/env sh
+readonly BREAK_INTERVAL=900
+readonly LOOP_TIMES=4
+
+for (( i = 0; i < LOOP_TIMES - 1; i++ )); do
+	sleep $BREAK_INTERVAL
+  say "You should move a bit"
+done
+
+sleep $BREAK_INTERVAL
+if hash terminal-notifier &> /dev/null; then
+  echo "It's time for a break" | terminal-notifier -sound default
+fi
 say "It's time for a break"
